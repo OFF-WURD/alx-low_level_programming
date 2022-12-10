1ba66ba1d843e22fadf1922f37e02b7dacf8fda0
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-10 04:41:37 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
 */
 int main(void)
 {
-  char string[] = "\"Programming is like building a multilingual puzzle";
-  puts(string);
-  return (0);
+ char string[] = "\"Programming is like building a multilingual puzzle";
+ puts(string);
+ return (0);
 }
 
