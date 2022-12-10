479af475eb826fc342672176bb3c01acaf271c37
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-10 06:13:52 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("size of a char:%2d byte(s)\n", sizeof(char));
 printf("size of an int:%2d byte(s)\n", sizeof(int));
 printf("size of a long int:%2d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %2d byte(s)\n", sizeof(double));
-printf("size of a double:%2d byte(s)\n", sizeof(float));
+printf("size of a float:%2d byte(s)\n", sizeof(float));
 return (0);
 }
