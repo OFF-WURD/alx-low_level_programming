99140c0272328c0259b277239239893f449d4634
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-14 00:38:17 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,10 @@
 *headers goes here
 *
 * main: Entry point
+*
+*Description: Display the type of n (positive, zero, negative)
+*
+*Return: 0 always(success)
 */
 int main(void)
 {
@@ -13,9 +17,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* my code start here */
-	if(n > 0)
+	if (n > 0)
 		printf("%i is positive\n", n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
