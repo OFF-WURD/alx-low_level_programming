d381e343e5a1dcbe181cf7c5d15a28c4bd022fad
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-14 05:50:06 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,5 +5,14 @@
 * Description - printing all single digit of base 10
 *
 * Return: 0 always (success)
+int main(void)
+{
+  int n = 0;
 
-
+  while (n < 10)
+  {
+    printf("%i\n", n);
+    n++;
+  }
+  return (0);
+}
