dac50c0090ffc8d58d24070d0094fb4eff53274d
OFF-WURLD <dfunsho72@gmail.com>
2022-12-14 18:53:08 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*Description - it print all alphabet in lower case 10x
+*
+*Return - 0 always(Success)
+*/
+void print_alphabet_10x(void)
+{
+	char ch = 0;
+	int count;
+
+	while (ch
