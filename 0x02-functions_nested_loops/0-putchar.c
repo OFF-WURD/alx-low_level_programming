6d406c2442297b582e19588a98cf97964b6241d3
OFF-WURLD <dfunsho72@gmail.com>
2022-12-14 16:39:34 -0800

    Function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description - a function that will print _putchar
+*
+* Return: 0 always (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int cha;
+
+	for(cha = 0; cha < 8; cha++)
+		_putchar(str[cha]);
+	_putchar('\n');
+
+	return (0);
+}
