9cb3bdb473afb773283001377294d4b10fa3e004
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-15 05:32:14 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,27 @@
 #include "main.h"
 /**
+*print_sign - prints the sign of a number
 *
+*@n: number being tested
+*
+* Return: Always 0.
 */
 int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
-	else if ( n == 0)
+	}
+	else if (n == 0)
+	{
 		return (0);
 		_putchar('0')
-	else if ( n < 0)
+	}
+	else if (n < 0)
+	{
 		return(-1);
-		putchar('-');
+		_putchar('-');
+	}
+	return (0);
 }
