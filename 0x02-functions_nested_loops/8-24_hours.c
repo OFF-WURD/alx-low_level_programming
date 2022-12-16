e4ede2fb41b44ed87c4ba14c7505619a1b1e35a1
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-16 22:43:37 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		while (i < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar(( j % 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(':');
+			_putchar((i / 10) + '0');
+			_putchar(( i % 10) + '0');
 			_putchar('\n');
 			++i;
 		}
