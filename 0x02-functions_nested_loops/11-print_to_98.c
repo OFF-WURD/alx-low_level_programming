ba496f059203abb7c40b07219ebf5ff0dd040e0e
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-17 03:46:07 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
 *98 - print any number and end it with 98
-*
 */
+void print_to_98(int n)
+{
+int i, j;
+
+if (n <= 98)
+{
+for (i = n; i <= 98; ++i)
+{
+if (i != 98)
+{
+printf("%d,\n", i);
+}
+else if ( i == 98)
+{
+printf("%d, \n", i);
+}
+}
+}
