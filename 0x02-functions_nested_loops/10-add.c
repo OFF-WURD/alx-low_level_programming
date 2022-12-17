83979a3554835e5e718d46d2d068ddd2541b9a0d
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-17 02:54:42 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,6 +7,19 @@ int add(int n1, int n2)
 int p;
 
 p = n1 + n2;
-if (p < 10)
+if (p < 0)
 {
-_putchar('p'
+_putchar('-');
+_putchar(p + '-');
+}
+else if (p >= 0 || p <= 9)
+{
+_putchar(p +'0');
+}
+else if (p >= 10)
+{
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+return (p);
+}
