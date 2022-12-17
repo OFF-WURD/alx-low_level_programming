fd449ef42c09caf957dbe360f9b732320067c8a8
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2022-12-17 01:10:00 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,30 @@ void times_table(void)
 {
 int a, b, c;
 
-for (a=0; a < 10; a++)
+for (a = 0; a < 10; a++)
 {
-for (b=0; a < 10; b++)
+for (b = 0; a < 10; b++)
 {
 c = a * b;
+if (b == 0)
+{
+_putchar( c + '0');
 }
-_putchar(c + '0');
+if ( c < 10 && b !=0)
+{
+ _putchar(',');
+ _putchar(' ');
+ _putchar(' ');
+ _putchar(c + '0');
+}
+else if ( c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
 }
 }
